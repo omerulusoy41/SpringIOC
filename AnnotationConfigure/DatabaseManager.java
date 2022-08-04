9f0be0ca3240d26b7bdd97b830b3cb1291411936
Omer Faruk Ulusoy <ulusoyomerfaruk29@gmail.com>
2022-08-04 11:16:30 +0300

    @Component(NAME) ozelligi kullanildi

--- AnnotationConfigure/DatabaseManager.java
+++ AnnotationConfigure/DatabaseManager.java
@@ -3,7 +3,7 @@ package AnnotationConfigure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("Manager")
 public class DatabaseManager {
 	
 //@Autowired
--- AnnotationConfigure/Main.java
+++ AnnotationConfigure/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("resource/ikinci.xml");
-		DatabaseManager manager=context.getBean("databaseManager",DatabaseManager.class);
+		DatabaseManager manager=context.getBean("Manager",DatabaseManager.class);
 		manager.yaz();
 	}
 
