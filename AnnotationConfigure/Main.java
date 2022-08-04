85e758d5146ce2c21149a3dcd0f11786e35d922d
Omer Faruk Ulusoy <ulusoyomerfaruk29@gmail.com>
2022-08-04 13:28:30 +0300

    SpelExpression methodu eklendi

--- AnnotationConfigure/Main.java
+++ AnnotationConfigure/Main.java
@@ -2,13 +2,29 @@ package AnnotationConfigure;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("resource/ikinci.xml");
-		DatabaseManager manager=context.getBean("Manager",DatabaseManager.class);
-		manager.yaz();
+//		ApplicationContext context = new ClassPathXmlApplicationContext("resource/ikinci.xml");
+//		DatabaseManager manager=context.getBean("Manager",DatabaseManager.class);
+//		manager.yaz();
+		spelExample();
+		
+     }
+	public static void spelExample() {
+		ExpressionParser parser=new SpelExpressionParser();
+		
+		Expression exp=parser.parseExpression("10==10");
+		boolean c=(Boolean)exp.getValue();
+		System.out.println(c);
+		
+		exp=parser.parseExpression("new String('HELLO World').toUpperCase()");
+		String name=(String)exp.getValue();
+		System.out.println(name);
 	}
 
 }
