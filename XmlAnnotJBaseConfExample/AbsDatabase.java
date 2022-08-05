38f86584b9ee933acd01e5e1665233b9a41cabf7
Omer Faruk Ulusoy <ulusoyomerfaruk29@gmail.com>
2022-08-05 16:12:27 +0300

    XML dosyasi ile bean ve attribute injectionlari yapildi

--- XmlAnnotJBaseConfExample/AbsDatabase.java
+++ XmlAnnotJBaseConfExample/AbsDatabase.java
@@ -3,9 +3,24 @@ package XmlAnnotJBaseConfExample;
 public abstract class AbsDatabase implements Database{
 	public String name;
 	public String connectWord;
+	public String[] ports;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setConnectWord(String connectWord) {
+		this.connectWord = connectWord;
+	}
+	public void setPorts(String[] ports) {
+		this.ports=ports;
+	}
 	@Override
 	public void info() {
 		System.out.println("name="+name);
 		System.out.println("CoonectWord="+connectWord);
+		System.out.print("ports:");
+		for(String p:ports) {
+			System.out.print(p+"-");
+		}
 	}
 }
--- XmlAnnotJBaseConfExample/Main.java
+++ XmlAnnotJBaseConfExample/Main.java
@@ -1,10 +1,15 @@
 package XmlAnnotJBaseConfExample;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
-		DatabaseManager manager=new DatabaseManager(new MySqlDatabase());
+		ApplicationContext context=new ClassPathXmlApplicationContext("resource/example.xml");
+		DatabaseManager manager=context.getBean("manager",DatabaseManager.class);
 		manager.connection();
+		manager.info();
 	}
 
 }
