e7e6839fc44f1b547046368e2b0353a87e7e7f76
Omer Faruk Ulusoy <ulusoyomerfaruk29@gmail.com>
2022-08-05 14:27:59 +0300

    Bu classda @Import anotasyonu ile .class ile olusturuldu

--- JavaBasedConfigure/Main.java
+++ JavaBasedConfigure/Main.java
@@ -3,9 +3,11 @@ package JavaBasedConfigure;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@ComponentScan(basePackageClasses =Configure.class)
+//@ComponentScan(basePackages = "JavaBasedConfigure")
+@Import(JavaBasedConfigure.Configure.class)
 public class Main {
 
 	public static void main(String[] args) {
